cd81cf0689f609f8094eee0ac674bbccecc82030
Matt Hibbs <mhibbs@trinity.edu>
2022-04-08 13:08:58 -0500

    Added iterator to Seq

--- src/main/scala/cs2/adt/LinkedSeq.scala
+++ src/main/scala/cs2/adt/LinkedSeq.scala
@@ -2,10 +2,11 @@ package cs2.adt
 
 class LinkedSeq[A] extends Seq[A] {
   private class Node (var data:A, var next:Node)
-  private var head:Node = null
+  private var hed:Node = null
+  private var len:Int = 0
 
   private def getNode(idx:Int):Node = {
-    var rover:Node = head
+    var rover:Node = hed
     for(ctr <- 0 until idx) {
       rover = rover.next
     }
@@ -20,20 +21,37 @@ class LinkedSeq[A] extends Seq[A] {
   }
   def insert(idx:Int, elem:A) = {
     if(idx == 0) {
-      head = new Node(elem, head)
+      hed = new Node(elem, hed)
     } else {
       val oneBefore = getNode(idx-1)
       oneBefore.next = new Node(elem, oneBefore.next)
     }
+    len += 1
   }
   def remove(idx:Int) = {
     if(idx == 0) {
-      head = head.next
+      hed = hed.next
     } else {
       val oneBefore = getNode(idx - 1)
       oneBefore.next = oneBefore.next.next
     }
+    len -= 1
   }
-  def length():Int = ???
+  def length():Int = len
 
+  def iterator:Iterator[A] = {
+    new Iterator[A]() {
+      var current:Node = hed
+
+      def hasNext:Boolean = { current != null }
+      def next():A = {
+        val ret = current.data
+        current = current.next
+        ret
+      }
+    }
+  }
 }
+
+
+
--- src/main/scala/cs2/adt/Seq.scala
+++ src/main/scala/cs2/adt/Seq.scala
@@ -1,11 +1,12 @@
 package cs2.adt
 
-abstract class Seq[A] {
+abstract class Seq[A] extends Iterable[A] {
   def get(idx:Int):A
   def set(idx:Int, elem:A)
   def insert(idx:Int, elem:A)
   def remove(idx:Int)
   def length():Int
+  def iterator:Iterator[A]
 }
 
 object Seq {
@@ -21,6 +22,17 @@ object Seq {
     for(i <- 0 until s.length()) {
       println(s.get(i))
     }
+
+    var it = s.iterator
+    while(it.hasNext) {
+      println(it.next)
+    }
+
+    it = s.iterator
+    it.foreach(println)
+
+    for(x <- s) { println(x) }
+
   }
 
 }
