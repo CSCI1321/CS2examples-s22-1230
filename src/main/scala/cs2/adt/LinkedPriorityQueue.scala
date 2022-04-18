25b7b23c6a4d6b410479c2872e8b82edecbb4785
Matt Hibbs <mhibbs@trinity.edu>
2022-04-18 13:24:10 -0500

    Finished LinkedPriorityQueue 2 ways

--- src/main/scala/cs2/adt/LinkedPriorityQueue.scala
+++ src/main/scala/cs2/adt/LinkedPriorityQueue.scala
@@ -5,9 +5,48 @@ class LinkedPriorityQueue[A <% Ordered[A]] extends PriorityQueue[A] {
   private var head:Node = null
 
   def add(elem:A):Unit = {
+    if(head == null || elem > head.data) {
+      head = new Node(elem, head)
+    } else {
+      var rover:Node = head
+      while(rover.next != null && rover.next.data >= elem) {
+        rover = rover.next
+      }
+      rover.next = new Node(elem, rover.next)
+    }
+  }
+  def get():A = { 
+    val ret = head.data
+    head = head.next
+    ret
+  }
+  def peek():A = { head.data }
+
+  /*
+  def add(elem:A):Unit = {
     head = new Node(elem, head)
   }
-  def get():A = ???
+  def get():A = {
+    var rover = head
+    var roversTail:Node = null
+    var biggest = head.data
+    var obb:Node = null
+
+    while(rover != null) {
+      if(rover.data > biggest) {
+        biggest = rover.data
+        obb = roversTail
+      }
+      roversTail = rover
+      rover = rover.next
+    }
+    if(obb == null) {
+      head = head.next
+    } else {
+      obb.next = obb.next.next
+    }
+    biggest
+  }
   def peek():A = {
     var biggest:A = head.data
     var rover:Node = head
@@ -17,6 +56,7 @@ class LinkedPriorityQueue[A <% Ordered[A]] extends PriorityQueue[A] {
     }
     biggest
   }
+  */
   def isEmpty():Boolean = { head == null }
 }
 
